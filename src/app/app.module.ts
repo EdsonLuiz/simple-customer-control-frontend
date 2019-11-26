0da4dd8d2848f6a0df52ca4d36935f2c87aeaa54
Edson Luiz Ribeiro Rodrigues <edsonrodrigues@icloud.com>
2019-11-26 15:27:55 -0300

    💩 Add a hardcoded list of clientes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { DiretivaComponent } from './diretiva/diretiva.component';
+import { ClientesComponent } from './clientes/clientes.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    DiretivaComponent
+    DiretivaComponent,
+    ClientesComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/clientes/cliente.ts
@@ -0,0 +1,8 @@
+
+export class Cliente {
+  id: number;
+  nome: string;
+  sobrenome: string;
+  createdAt: string;
+  email: string;
+}
