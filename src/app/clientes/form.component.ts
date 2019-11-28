817f9e9f303d001795c70abb1ecc5f06ca038f49
Edson Luiz Ribeiro Rodrigues <edsonrodrigues@icloud.com>
2019-11-28 13:08:00 -0300

    ♻️ Persist form data
    This will make use of POST /api/clientes to save Cliente in database

--- src/app/clientes/form.component.ts
+++ src/app/clientes/form.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Cliente } from './cliente';
+import { ClienteService } from './cliente.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-form',
@@ -10,15 +12,16 @@ export class FormComponent implements OnInit {
   private cliente: Cliente = new Cliente();
   private titulo: string = 'Criar cliente'
 
-  constructor() { }
+  constructor(private clienteService: ClienteService, private router: Router) { }
 
   ngOnInit() {
   }
 
 
   public create(): void{
-    console.log("OK")
-    console.log(this.cliente)
+    this.clienteService.store(this.cliente).subscribe(
+      response => this.router.navigate(['/clientes'])
+    )
   }
 
 }
