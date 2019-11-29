ece275f33e3d9a5d8b0b12e08a9b92aa171de940
Edson Luiz Ribeiro Rodrigues <edsonrodrigues@icloud.com>
2019-11-29 10:51:05 -0300

    ✨ Add capability to delete Cliente

--- src/app/clientes/clientes.component.ts
+++ src/app/clientes/clientes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Cliente } from './cliente';
 import { ClienteService } from './cliente.service';
+import Swal from 'sweetalert2'
 
 
 @Component({
@@ -19,4 +20,37 @@ export class ClientesComponent implements OnInit {
     );
   }
 
+  delete(cliente: Cliente): void {
+    const swalWithBootstrapButtons = Swal.mixin({
+      customClass: {
+        confirmButton: 'btn btn-success',
+        cancelButton: 'btn btn-danger'
+      },
+      buttonsStyling: false
+    })
+
+    swalWithBootstrapButtons.fire({
+      title: 'Está seguro?',
+      text: `Deseja remove o cliente ${cliente.nome} ${cliente.sobrenome}`,
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonText: 'Sim, remova!',
+      cancelButtonText: 'Não, cancele!',
+      reverseButtons: true
+    }).then((result) => {
+      if (result.value) {
+        this.clienteService.delete(cliente.id).subscribe(
+
+          response =>  {
+            this.clientes = this.clientes.filter( cli => cli !== cliente)
+            swalWithBootstrapButtons.fire(
+              'Removido!',
+              'O cliente foi removido.',
+              'success'
+            )
+          }
+        )
+      }
+    })
+  }
 }
